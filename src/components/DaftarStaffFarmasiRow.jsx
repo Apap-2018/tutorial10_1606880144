1fd3d6e1e266b3bec63ceb70f38435ef705b1d3e
nathanael.lemuella <nathanael.lemuella@ui.ac.id>
2018-12-05 19:46:06 +0700

    Tutor 10 done

---
+++ src/components/DaftarStaffFarmasiRow.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export const DaftarStaffFarmasiRow = (props) => {
+    return (
+        <tbody>
+            {props.listStaffFarmasi.map(staffFarmasi => {
+                return (
+                    <tr key={staffFarmasi.id}>
+                        <td>{staffFarmasi.nama}</td>
+                        <td>{staffFarmasi.jenis}</td>
+                    </tr>
+                )
+            })}
+        </tbody>
+    )
+}
